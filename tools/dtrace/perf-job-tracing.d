149738d4985e801c9683e3d40c6bbad4f6df54cf
Matt Keeter <matt@oxide.computer>
2024-02-15 15:18:35 -0500

    Updates to perf-job-tracing.c

--- tools/dtrace/perf-job-tracing.d
+++ tools/dtrace/perf-job-tracing.d
@@ -91,6 +91,75 @@ crucible_upstairs*:::up-guest-done
 crucible_upstairs*:::up-guest-done
 /gw_reserved[arg0]/
 {
+    printf("ds_id: %u gw_id: %u\n", arg1, arg0);
+    start = gw_reserved[arg0];
+    printf("reserved -> start: %u\n", gw_start[arg0] - start);
+    printf("         -> bind:  %u\n", gw_bind[arg0] - start);
+
+    printf("client 0 tx\n");
+    printf("  -> in progress:         %u\n", up_in_progress[arg1, 0] - start);
+    printf("  -> client message sent: %u\n", up_client_message_sent[arg1, 0] - start);
+    printf("  -> io task sending:     %u\n", up_io_sending_message[arg1, 0] - start);
+    printf("  -> io task sent:        %u\n", up_io_message_sent[arg1, 0] - start);
+
+    printf("client 1 tx\n");
+    printf("  -> in progress:         %u\n", up_in_progress[arg1, 1] - start);
+    printf("  -> client message sent: %u\n", up_client_message_sent[arg1, 1] - start);
+    printf("  -> io task sending:     %u\n", up_io_sending_message[arg1, 1] - start);
+    printf("  -> io task sent:        %u\n", up_io_message_sent[arg1, 1] - start);
+
+    printf("client 2 tx\n");
+    printf("  -> in progress:         %u\n", up_in_progress[arg1, 2] - start);
+    printf("  -> client message sent: %u\n", up_client_message_sent[arg1, 2] - start);
+    printf("  -> io task sending:     %u\n", up_io_sending_message[arg1, 2] - start);
+    printf("  -> io task sent:        %u\n", up_io_message_sent[arg1, 2] - start);
+
+    a = up_io_message_sent[arg1, 0] - start;
+    b = up_io_message_sent[arg1, 1] - start;
+    c = up_io_message_sent[arg1, 2] - start;
+    io_sent_by = a > b ? (a > c ? a : c) : (b > c ? c : b);
+    printf("all IO sent at %u\n", io_sent_by);
+    @io_sent = quantize(io_sent_by);
+
+    if (up_io_completion[arg1, 0]) {
+        @io_reply = avg(up_io_message_rx[arg1, 0] - up_io_message_sent[arg1, 0]);
+        printf("client 0 rx\n");
+        printf("  -> io message rx: %u\n", up_io_message_rx[arg1, 0] - start);
+        printf("  -> ds select:     %u\n", up_ds_select[arg1, 0] - start);
+        printf("  -> ds deferring:  %u\n", up_deferring[arg1, 0] - start);
+        printf("  -> ds defer done: %u\n", up_defer_done[arg1, 0] - start);
+        printf("  -> client rx:     %u\n", up_client_message_rx[arg1, 0] - start);
+        printf("  -> io completion: %u\n", up_io_completion[arg1, 0] - start);
+    }
+
+    if (up_io_completion[arg1, 1]) {
+        @io_reply = avg(up_io_message_rx[arg1, 1] - up_io_message_sent[arg1, 1]);
+        printf("client 1 rx\n");
+        printf("  -> io message rx: %u\n", up_io_message_rx[arg1, 1] - start);
+        printf("  -> ds select:     %u\n", up_ds_select[arg1, 1] - start);
+        printf("  -> ds deferring:  %u\n", up_deferring[arg1, 1] - start);
+        printf("  -> ds defer done: %u\n", up_defer_done[arg1, 1] - start);
+        printf("  -> client rx:     %u\n", up_client_message_rx[arg1, 1] - start);
+        printf("  -> io completion: %u\n", up_io_completion[arg1, 1] - start);
+    }
+
+    if (up_io_completion[arg1, 2]) {
+        @io_reply = avg(up_io_message_rx[arg1, 2] - up_io_message_sent[arg1, 2]);
+        printf("client 2 rx\n");
+        printf("  -> io message rx: %u\n", up_io_message_rx[arg1, 2] - start);
+        printf("  -> ds select:     %u\n", up_ds_select[arg1, 2] - start);
+        printf("  -> ds deferring:  %u\n", up_deferring[arg1, 2] - start);
+        printf("  -> ds defer done: %u\n", up_defer_done[arg1, 2] - start);
+        printf("  -> client rx:     %u\n", up_client_message_rx[arg1, 2] - start);
+        printf("  -> io completion: %u\n", up_io_completion[arg1, 2] - start);
+    }
+
+    printf("-> gw-done: %u\n", gw_done[arg0] - start);
+    printf("-> done:    %u\n", guest_done[arg0] - start);
+    @total_time = avg(guest_done[arg0] - start);
+
+    printf("\n");
+
     gw_reserved[arg0] = 0;
     gw_start[arg0] = 0;
     gw_bind[arg0] = 0;
